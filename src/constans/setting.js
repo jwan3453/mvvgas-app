78e3f4164aa5b7c50d44a3b86dd29ff1a52654b4
jacky wang <jackyloop@outlook.com>
2019-06-25 11:55:07 +0800

    build realease apk

--- src/constans/setting.js
+++ src/constans/setting.js
@@ -1,1 +1,2 @@
-export const API_ROOT = 'http://mvv-fsa-app-dev.us-west-2.elasticbeanstalk.com';
+export const API_ROOT = 'http://mvv-fsa-app-dev.us-west-2.elasticbeanstalk.com/';
+//export const API_ROOT = 'http://47.112.214.238'
